4df2fac2ab337ab9280ae41da583919bf40d913c
George-Beyrouti <georges.beyrouti@gmail.com>
2023-10-06 20:03:30 +0200

    accessiblity

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -59,7 +59,7 @@ const Contact = () => {
 
             <button type='submit' className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Lets talk</button>
 
-            <h4 className='p-3 my-4 bg-transparent border-2 rounded-md text-white' placeholder='message confirmation' >message confirmation status: {confirmation}</h4>
+            <p className='p-3 my-4 bg-transparent border-2 rounded-md text-white' placeholder='message confirmation' >message confirmation status: {confirmation}</p>
           </form>
         </div>
       </div>
