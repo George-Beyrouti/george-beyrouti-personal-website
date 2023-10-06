6631ff678bbe75805929f1767bfa9fbd7198fa5c
George-Beyrouti <georges.beyrouti@gmail.com>
2023-10-06 19:30:31 +0200

    accessiblity

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports ={
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",      
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://george-beyrouti-personal-website.vercel.app/" || "http://localhost:3000",      
     generateRobotsTxt: true,
     generateIndexSitemap: false,
 }
